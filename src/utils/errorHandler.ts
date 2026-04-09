9b63fb2a4cec481b12d540c4aaa799441c7b6f3f
Lina <linabell@mail.com>
2026-04-09 08:43:28 -0700

    Prevent crashes from API timeouts

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -12,30 +12,31 @@ export const errorHandler = ({
   res?: any;
 }) => {
   try {
-    if (process.env.NODE_ENV === "development") console.log("❌ Error:", error);
-    else {
-      const reqQueryParams = req?.query;
-      if (reqQueryParams?.interactiveNonce) delete reqQueryParams.interactiveNonce;
+    if (process.env.NODE_ENV === "development") console.error("❌ Error:", error);
 
-      console.error(
-        JSON.stringify({
-          errorContext: {
-            message,
-            functionName,
-          },
-          requestContext: {
-            requestId: req?.id,
-            reqQueryParams,
-            reqBody: req?.body,
-          },
-          error: JSON.stringify(error, Object.getOwnPropertyNames(error)),
-        }),
-      );
-    }
-    if (res) return res.status(error.status || 500).send({ error, message, success: false });
+    const reqQueryParams = req?.query;
+    if (reqQueryParams?.interactiveNonce) delete reqQueryParams.interactiveNonce;
+
+    console.error(
+      JSON.stringify({
+        errorContext: {
+          message,
+          functionName,
+        },
+        requestContext: {
+          requestId: req?.id,
+          reqQueryParams,
+          reqBody: req?.body,
+        },
+        error: JSON.stringify(error, Object.getOwnPropertyNames(error)),
+      }),
+    );
+
+    if (res && !res.headersSent) return res.status(error.status || 500).send({ error, message, success: false });
     return { error };
   } catch (e) {
     console.error("❌ Error printing the logs", e);
-    return res.status(500).send({ error: e, message, success: false });
+    if (res && !res.headersSent) return res.status(500).send({ error: e, message, success: false });
+    return { error: e };
   }
 };
